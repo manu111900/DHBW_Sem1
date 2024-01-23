9b2ddd7b827bd9d60640a9f60e7c09f274546369
Schmollinger <schmolh@emea.corpdir.net>
2024-01-23 09:41:51 +0100

    Update pd_05.py

--- pd_05.py
+++ pd_05.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -5,9 +6,15 @@ import matplotlib.pyplot as plt
 # pip install matplotlib
 # pip install openpyxl
 
+'''
 df = pd.DataFrame({'length': [1.5, 0.5, 1.2, 0.9, 3],
                   'width': [0.7, 0.2, 0.15, 0.2, 1.1]},
                   index=['pig', 'rabbit', 'duck', 'chicken', 'horse'])
+'''
+
+df = pd.read_excel("pd_04_to_excel.xlsx")
+print(df)
+df=df.set_index("Town")
 
 print(df)
 
